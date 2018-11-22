fb5c01acd9be4ac3c5f5694641736d2fc04b92c6
Alan Vieceli <alansvieceli@gmail.com>
2018-11-22 10:58:52 -0200

    Adicionando testes

--- src/main/java/com/vieceli/teste/integracao/controllers/HelloController.java
+++ src/main/java/com/vieceli/teste/integracao/controllers/HelloController.java
@@ -1,14 +1,18 @@
 package com.vieceli.teste.integracao.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
+	@Value("${application.title}")
+	private String applicationTitle;
+	
 	@RequestMapping("/")
 	public String index() {
-		return "Teste Integração Github / Travis CI / Heorku [OK]";
+		return applicationTitle;
 	}
 
 }
--- src/test/java/com/vieceli/teste/integracao/TesteIntegracaoApplicationTests.java
+++ src/test/java/com/vieceli/teste/integracao/TesteIntegracaoApplicationTests.java
@@ -2,15 +2,31 @@ package com.vieceli.teste.integracao;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class TesteIntegracaoApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+	
+	@Value("${application.title}")
+	private String applicationTitle;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		this.mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string(applicationTitle));
 	}
 
 }
